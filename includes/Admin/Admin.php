e2e93650db10a8c4d861fdb91001331ed8da3654
Prajjwal Poudel <iamprazol@gmail.com>
2022-05-13 14:55:20 +0545

    Avatar upload settings added

--- includes/Admin/Admin.php
+++ includes/Admin/Admin.php
@@ -35,6 +35,7 @@ class Admin {
 	 */
 	private function init_hooks() {
 		add_action( 'admin_menu', array( $this, 'wpmake_user_avatar_menu' ), 68 );
+		add_action( 'admin_init', array( $this, 'wpmake_user_avatar_setting' ) );
 	}
 
 	/**
@@ -55,11 +56,133 @@ class Admin {
 
 	}
 
-
 	/**
 	 *  Init the User Avatar Settings page.
 	 */
 	public function wpmake_user_avatar_settings_page() {
-		echo '<h1>Hello</h1>';
+		?>
+		<form action='options.php' method='post'>
+			<table class="form-table">
+				<tbody>
+					<?php
+					settings_fields( 'wpmake_user_avatar_settings' );
+					do_settings_sections( 'wpmake_user_avatar_settings' );
+					submit_button();
+					?>
+				</tbody>
+			</table>
+		</form>
+		<?php
+
+	}
+
+	/**
+	 *  Init the User Avatar Settings.
+	 */
+	public function wpmake_user_avatar_setting() {
+		register_setting( 'wpmake_user_avatar_settings', 'wpmake_user_avatar_settings' );
+
+		add_settings_section(
+			'wpmake_user_avatar_setting_section',
+			__( 'Settings', 'wpmake-user-avatar' ),
+			'',
+			'wpmake_user_avatar_settings'
+		);
+
+		add_settings_field(
+			'wpmake_user_avatar_settings_max_size',
+			__( 'Max Avatar Size Allowed', 'wpmake-user-avatar' ),
+			array( $this, 'wpmake_user_avatar_setting_max_size_callback' ),
+			'wpmake_user_avatar_settings',
+			'wpmake_user_avatar_setting_section'
+		);
+
+		add_settings_field(
+			'wpmake_user_avatar_settings_allowed_file_type',
+			__( 'Allowed File Type', 'wpmake-user-avatar' ),
+			array( $this, 'wpmake_user_avatar_settings_allowed_file_type_callback' ),
+			'wpmake_user_avatar_settings',
+			'wpmake_user_avatar_setting_section'
+		);
+
+		add_settings_field(
+			'wpmake_user_avatar_settings_capture_picture',
+			__( 'Enable cropping interface', 'wpmake-user-avatar' ),
+			array( $this, 'wpmake_user_avatar_settings_capture_picture_callback' ),
+			'wpmake_user_avatar_settings',
+			'wpmake_user_avatar_setting_section'
+		);
+
+		add_settings_field(
+			'wpmake_user_avatar_settings_cropping_interface',
+			__( 'Enable cropping interface', 'wpmake-user-avatar' ),
+			array( $this, 'wpmake_user_avatar_settings_cropping_interface_callback' ),
+			'wpmake_user_avatar_settings',
+			'wpmake_user_avatar_setting_section'
+		);
+	}
+
+	/**
+	 *  Max avatar size setting.
+	 */
+	public function wpmake_user_avatar_setting_max_size_callback() {
+		$options  = get_option( 'wpmake_user_avatar_settings' );
+		$max_size = '1024';
+		if ( isset( $options['max_size'] ) ) {
+			$max_size = esc_html( $options['max_size'] );
+		}
+
+		echo '<input name="user_registration_pro_general_post_submission_settings" id="user_registration_pro_general_post_submission_settings" type="text" value="' . esc_attr( $max_size ) . '" class="" placeholder="" />';
+		echo '<p style="font-style: italic;">' . esc_html__( 'Maximum avatar size allowed for upload. Enter file size in Kb. Leave the field empty for upload without restriction', 'wpmake-user-avatar' ) . '</p>';
+	}
+
+	/**
+	 *  Allowed file type setting.
+	 */
+	public function wpmake_user_avatar_settings_allowed_file_type_callback() {
+		$options  = get_option( 'wpmake_user_avatar_settings' );
+		$allowed_file_type = '';
+		if ( isset( $options['allowed_file_type'] ) ) {
+			$max_size = esc_html( $options['allowed_file_type'] );
+		}
+		echo "<select name='allowed_file_type[]' multiple='multiple'>
+		<option value='image/jpg'>" . esc_html__( 'JPG', 'wpmake-user-avatar' ) . "</option>
+		<option value='image/jpeg'>" . esc_html__( 'JPEG', 'wpmake-user-avatar' ) . "</option>
+		<option value='image/gif'>" . esc_html__( 'GIF', 'wpmake-user-avatar' ) . "</option>
+		<option value='image/png'>" . esc_html__( 'PNG', 'wpmake-user-avatar' ) . '</option>
+				</select>';
+		echo '<p style="font-style: italic;">' . esc_html__( 'Choose valid file types allowed for avatar upload', 'wpmake-user-avatar' ) . '</p>';
+	}
+
+	/**
+	 *  Cropping interface.
+	 */
+	public function wpmake_user_avatar_settings_cropping_interface_callback( $args ) {
+
+		$options = get_option( 'wpmake_user_avatar_settings' );
+
+		$checkbox = '';
+		if ( isset( $options['checkbox'] ) ) {
+			$checkbox = esc_html( $options['checkbox'] );
+		}
+
+		echo '<input type="checkbox"  name="wpmake_user_avatar_settings[checkbox]" value="1"' . checked( 1, $checkbox, false ) . '/>';
+		echo '<p style="font-style: italic;">' . esc_html__( 'This option will enable avatar cropping interface', 'wpmake-user-avatar' ) . '</p>';
+	}
+
+	/**
+	 *  Capture picture.
+	 */
+	public function wpmake_user_avatar_settings_capture_picture_callback( $args ) {
+
+		$options = get_option( 'wpmake_user_avatar_settings' );
+
+		$checkbox = '';
+		if ( isset( $options['checkbox'] ) ) {
+			$checkbox = esc_html( $options['checkbox'] );
+		}
+
+		echo '<input type="checkbox"  name="wpmake_user_avatar_settings[checkbox]" value="1"' . checked( 1, $checkbox, false ) . '/>';
+		echo '<p style="font-style: italic;">' . esc_html__( 'This option will enable taking picture using webcam. Note that your site must be secure', 'wpmake-user-avatar' ) . '</p>';
 	}
 }
